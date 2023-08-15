01598f27e7608df044d104da4107ca944df28960
Justin Kurpeik <j.kurpeik@gmail.com>
2023-08-15 08:12:40 +0200

    chessboard recognition

---
+++ camera_calibration/camera_calibration.py
@@ -0,0 +1,105 @@
+# Copyright (C) 2022 NG:ITL
+
+import cv2
+import numpy as np
+import pynng
+from image_sources import VideoFileSource, CameraStreamSource
+
+# pynng
+# address_rec_frame = "ipc:///tmp/RAAI/camera_frame.ipc"
+# frame_receiver = CameraStreamSource(address_rec_frame)
+
+#camera
+frame_receiver = cv2.VideoCapture(0)
+
+# Define the dimensions of checkerboard
+CHECKERBOARD = (5, 5)
+
+# stop the iteration when specified
+# accuracy, epsilon, is reached or
+# specified number of iterations are completed.
+criteria = (cv2.TERM_CRITERIA_EPS +
+            cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# Vector for 3D points
+threedpoints = []
+
+# Vector for 2D points
+twodpoints = []
+
+#  3D points real world coordinates
+objectp3d = np.zeros((1, CHECKERBOARD[0]
+                      * CHECKERBOARD[1],
+                      3), np.float32)
+objectp3d[0, :, :2] = np.mgrid[0:CHECKERBOARD[0],
+                      0:CHECKERBOARD[1]].T.reshape(-1, 2)
+prev_img_shape = None
+
+# Extracting path of individual image stored
+# in a given directory. Since no path is
+# specified, it will take current directory
+# jpg files alone
+
+while True:
+    # image = frame_receiver.read_new_frame()
+    reti, image = frame_receiver.read()
+
+    grayColor = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Find the chess board corners
+    # If desired number of corners are
+    # found in the image then ret = true
+    ret, corners = cv2.findChessboardCorners(
+        grayColor, CHECKERBOARD,
+        cv2.CALIB_CB_ADAPTIVE_THRESH
+        + cv2.CALIB_CB_FAST_CHECK +
+        cv2.CALIB_CB_NORMALIZE_IMAGE)
+    print(ret)
+
+    # If desired number of corners can be detected then,
+    # refine the pixel coordinates and display
+    # them on the images of checkerboard
+    if ret == True:
+        threedpoints.append(objectp3d)
+
+        # Refining pixel coordinates
+        # for given 2d points.
+        corners2 = cv2.cornerSubPix(
+            grayColor, corners, (11, 11), (-1, -1), criteria)
+
+        twodpoints.append(corners2)
+
+        # Draw and display the corners
+        image = cv2.drawChessboardCorners(image,
+                                          CHECKERBOARD,
+                                          corners2, ret)
+        cv2.imshow('imgcheck', image)
+        print("jo")
+
+    cv2.imshow('img', image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+
+h, w = image.shape[:2]
+
+# Perform camera calibration by
+# passing the value of above found out 3D points (threedpoints)
+# and its corresponding pixel coordinates of the
+# detected corners (twodpoints)
+ret, matrix, distortion, r_vecs, t_vecs = cv2.calibrateCamera(
+    threedpoints, twodpoints, grayColor.shape[::-1], None, None)
+
+# Displaying required output
+print(" Camera matrix:")
+print(matrix)
+
+print("\n Distortion coefficient:")
+print(distortion)
+
+print("\n Rotation Vectors:")
+print(r_vecs)
+
+print("\n Translation Vectors:")
+print(t_vecs)
